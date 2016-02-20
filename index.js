e3f2453db3e35b4cfbc392e4a4eb1198810bebe3
Kevin McGrath <kevin.mcgrath@sungardas.com>
2016-02-19 21:40:30 -0500

    add module exports

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var util = require('./lib/util');
+var resource = require('./lib/resource');
+var project = require('./lib/project');
 
 module.exports.util = util;
+module.exports.resource = resource;
+module.exports.project = project;
